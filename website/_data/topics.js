2f5eabe72de1e038b87bcd3b267aa29cdd90e745
dachafra <dachafra@gmail.com>
2021-05-25 10:30:47 +0200

    semantics2021 website

--- website/_data/topics.js
+++ website/_data/topics.js
@@ -3,20 +3,21 @@ const {query} = require('graphqlld-on-file');
 module.exports = async () => {
  let result = await query('../kg/data.nt',{
    "name": { "@id": "http://schema.org/name", "@singular": true },
+   "keyword": { "@id": "http://schema.org/keyword", "@singular": true},
    "CreativeWork": "http://schema.org/CreativeWork"
- },`{... on CreativeWork {name}}`);
+ },`{... on CreativeWork {name keyword}}`);
 
   const topics = {};
 
   result.forEach(r => {
-    const {name,} = r;
+    const {name, keyword} = r;
 
     if (!topics[name]) {
       topics[name] = [];
     }
 
+    topics[name].push(keyword);
   });
-
   result = [];
   const mainTopics = Object.keys(topics);
 
